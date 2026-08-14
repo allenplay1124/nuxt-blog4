0f5af22d574484539c322657046ec987874d3096
Allenplay <jailin1124@gmail.com>
2026-08-14 11:00:55 +0800

    新增 AppFooter 組件，整合使用者配置並顯示版權資訊

--- app/components/AppFooter.vue
+++ app/components/AppFooter.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+const config = useRuntimeConfig()
+</script>
 <template>
-    
+    <UFooter>
+        © 2026 <NuxtLink to="/" class="font-bold">{{ config.public.title }}</NuxtLink>. All rights reserved.
+    </UFooter>
 </template>
